d8c973c8f6235f036830063867edacb08fa0b9d7
3r11k <erikbatista1222@gmail.com>
2025-12-08 20:56:07 -0300

    Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/erik-rafa-s-gift-scrapbook">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
